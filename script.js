7f13b41a5d66e125d7bf20d113e8be35f20d760f
Lord Arcus <lordarcus11@gmail.com>
2024-06-01 11:42:39 -0400

    Made some changes in design

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var navLinks = document.getElementById("navLinks");
 function showMenu(){
+    navLinks.style.display  = "block";
     navLinks.style.right = "0";
+    navLinks.style.transition = "5s ease 3s";
 }
 function hideMenu(){
+    navLinks.style.display = "none";
     navLinks.style.right = "-200px";
+    navLinks.style.transition = "5s ease 3s";
 }
 
